3a88ad1402f900b8c31bd30d3a96792cfe467f6b
Infinity585 <89676623+Infinity585@users.noreply.github.com>
2022-09-05 02:07:04 -0700

    Init

---
+++ config.lua
@@ -0,0 +1,40 @@
+--[[
+=============================================================
+               Made By: Wakie Modifications 
+=============================================================
+--]]
+
+Config = {
+    Objects = {
+        [1] = { -- Example 1
+            Hash = 779917859, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(966.08, -1657.65, 29.29, 88.81) -- Vector 4 Coords for the props location
+        },
+        [2] = { -- Example 2
+            Hash = 779917859, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(970.02, -1653.2, 29.35, 7.23) -- Vector 4 Coords for the props location
+        },
+        [3] = { -- Gun Factory, Gun Bench
+            Hash = 765424411, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(1114.05, -2005.86, 35.5, 145.51) -- Vector 4 Coords for the props location
+        },
+        [4] = { -- Example 2
+        Hash = 4103028989, -- Prop Hash (Found Here: https://gtahash.ru/)
+        FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+        Coords = vector4(1113.65, -2002.92, 35.0, 235.85) -- Vector 4 Coords for the props location
+        },
+        [5] = { -- Example 2
+        Hash = 4103028989, -- Prop Hash (Found Here: https://gtahash.ru/)
+        FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+        Coords = vector4(1115.13, -2001.1, 35.44, 231.78) -- Vector 4 Coords for the props location
+        },
+        [6] = { -- Example 2
+        Hash = 144907061, -- Prop Hash (Found Here: https://gtahash.ru/)
+        FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+        Coords = vector4(2358.03, 3115.8, 48.21, 257.08) -- Vector 4 Coords for the props location
+        },
+    }
+}
